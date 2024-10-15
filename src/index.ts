7062b7793ad9fbfe69e0fab1d2eddce4751cff92
datdh <datdh@omegatheme.com>
2024-10-15 14:22:06 +0700

    create db connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("hello", 3);
+import { DataSource } from "typeorm";
+
+const main = async () => {
+  try {
+    const MysqlDataSource = new DataSource({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "test",
+      password: "",
+      database: "typeorm_learning",
+      entities: [
+        // ....
+      ],
+    });
+
+    console.log(MysqlDataSource.manager.find);
+  } catch (error) {
+    console.log("Connection error: ", error);
+    throw new Error("Connection error");
+  }
+};
+
+main();
