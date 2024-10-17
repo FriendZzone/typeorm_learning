8d96391846decefd2534ba5c417043af6dc0735c
datdh <datdh@omegatheme.com>
2024-10-17 14:35:13 +0700

    get all Clients

--- src/routes/get_client.ts
+++ src/routes/get_client.ts
@@ -1,6 +1,18 @@
 import express, { Request, Response } from "express";
 import { Client } from "../entities/Client";
+import { MoreThan } from "typeorm";
 const router = express.Router();
+
+router.get("/api/client", async (req: Request, res: Response) => {
+  const { balance } = req.query;
+  const clients = await Client.find({
+    where: {
+      balance: balance ? MoreThan(parseInt(balance as string)) : undefined,
+    },
+  });
+  res.status(200).json(clients);
+  return;
+});
 
 router.get("/api/client/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
